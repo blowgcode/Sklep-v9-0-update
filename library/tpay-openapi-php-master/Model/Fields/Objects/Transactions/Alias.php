0fe0a8e6a2e6ac7922b863ea6c6aee38a9cf0101
blowgcode <134099157+blowgcode@users.noreply.github.com>
2026-02-02 23:39:16 +0100

    Add files via upload

---
+++ library/tpay-openapi-php-master/Model/Fields/Objects/Transactions/Alias.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tpay\OpenApi\Model\Objects\Transactions;
+
+use Tpay\OpenApi\Model\Fields\Alias\Key;
+use Tpay\OpenApi\Model\Fields\Alias\Label;
+use Tpay\OpenApi\Model\Fields\Alias\Type;
+use Tpay\OpenApi\Model\Fields\Alias\Value;
+use Tpay\OpenApi\Model\Fields\Boolean;
+use Tpay\OpenApi\Model\Objects\Objects;
+use Tpay\OpenApi\Model\Objects\Transactions\Alias\Autopayment;
+
+class Alias extends Objects
+{
+    const OBJECT_FIELDS = [
+        'value' => Value::class,
+        'type' => Type::class,
+        'label' => Label::class,
+        'key' => Key::class,
+        'autopayment' => Autopayment::class,
+        'noDelay' => Boolean::class,
+    ];
+
+    /** @var Value */
+    public $value;
+
+    /** @var Type */
+    public $type;
+
+    /** @var Label */
+    public $label;
+
+    /** @var Key */
+    public $key;
+
+    /** @var Autopayment */
+    public $autopayment;
+
+    /** @var \Tpay\OpenApi\Model\Fields\Boolean */
+    public $noDelay;
+
+    public function getRequiredFields()
+    {
+        return [
+            $this->value,
+            $this->type,
+        ];
+    }
+}
---
+++ library/tpay-openapi-php-master/Model/Fields/Objects/Transactions/BlikPaymentData.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tpay\OpenApi\Model\Objects\Transactions;
+
+use Tpay\OpenApi\Model\Fields\BlikPaymentData\BlikToken;
+use Tpay\OpenApi\Model\Fields\BlikPaymentData\Type;
+use Tpay\OpenApi\Model\Fields\Boolean;
+use Tpay\OpenApi\Model\Objects\Objects;
+
+class BlikPaymentData extends Objects
+{
+    const OBJECT_FIELDS = [
+        'blikToken' => BlikToken::class,
+        'aliases' => Alias::class,
+        'type' => Type::class,
+        'refuseNoPayId' => Boolean::class,
+    ];
+
+    /** @var BlikToken */
+    public $blikToken;
+
+    /** @var Alias */
+    public $aliases;
+
+    /** @var Type */
+    public $type;
+
+    /** @var bool */
+    public $refuseNoPayId;
+}
