ad6757b4a51b064e613fa9437be9e7144bf7f2f3
blowgcode <134099157+blowgcode@users.noreply.github.com>
2026-02-03 00:27:36 +0100

    Add files via upload

---
+++ library/tpay-openapi-php-master/Model/Fields/Objects/RequestBody/Pay.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tpay\OpenApi\Model\Objects\RequestBody;
+
+use Tpay\OpenApi\Model\Fields\Pay\ApplePayPaymentData;
+use Tpay\OpenApi\Model\Fields\Pay\CardOnFile;
+use Tpay\OpenApi\Model\Fields\Pay\GooglePayPaymentData;
+use Tpay\OpenApi\Model\Fields\Pay\Method;
+use Tpay\OpenApi\Model\Identifiers\ChannelId;
+use Tpay\OpenApi\Model\Identifiers\GroupId;
+use Tpay\OpenApi\Model\Objects\Objects;
+use Tpay\OpenApi\Model\Objects\Transactions\BlikPaymentData;
+use Tpay\OpenApi\Model\Objects\Transactions\CardPaymentData;
+use Tpay\OpenApi\Model\Objects\Transactions\Recursive;
+use Tpay\OpenApi\Model\Objects\Transactions\TokenPaymentData;
+
+class Pay extends Objects
+{
+    const OBJECT_FIELDS = [
+        'groupId' => GroupId::class,
+        'channelId' => ChannelId::class,
+        'method' => Method::class,
+        'blikPaymentData' => BlikPaymentData::class,
+        'cardPaymentData' => CardPaymentData::class,
+        'tokenPaymentData' => TokenPaymentData::class,
+        'recursive' => Recursive::class,
+        'applePayPaymentData' => ApplePayPaymentData::class,
+        'googlePayPaymentData' => GooglePayPaymentData::class,
+        'cof' => CardOnFile::class,
+    ];
+
+    /** @var GroupId */
+    public $groupId;
+
+    /** @var ChannelId */
+    public $channelId;
+
+    /** @var Method */
+    public $method;
+
+    /** @var BlikPaymentData */
+    public $blikPaymentData;
+
+    /** @var CardPaymentData */
+    public $cardPaymentData;
+
+    /** @var TokenPaymentData */
+    public $tokenPaymentData;
+
+    /** @var Recursive */
+    public $recursive;
+
+    /** @var ApplePayPaymentData */
+    public $applePayPaymentData;
+
+    /** @var GooglePayPaymentData */
+    public $googlePayPaymentData;
+
+    /** @var CardOnFile */
+    public $cof;
+
+    public function getRequiredFields()
+    {
+        return [
+            $this->groupId,
+        ];
+    }
+}
